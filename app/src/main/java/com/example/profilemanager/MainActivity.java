01ba5947d1a2d37c4fcab1947fa1757866861589
SebastienColl <scoll010@uottawa.ca>
2020-10-14 16:30:52 -0400

    Implementation de l'activité secondaire
    -Ajout des images nécéssaires au changement de photo de profil
    -Organisation des élémentsdans l'activité secondaire à l'aide de LinearLayout
    -Ajout des fonctions nécéssaires au fonctionnement à la fonctionnalité de changement de photo.
    -Réorganisation du code.

--- app/src/main/java/com/example/profilemanager/MainActivity.java
+++ app/src/main/java/com/example/profilemanager/MainActivity.java
@@ -8,38 +8,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button openGoogleMapsButton;
-    EditText teamAddressField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        //Get the necessary buttons and text fields from the screen
-        openGoogleMapsButton = (Button)findViewById(R.id.openGoogleMapsButton);
-        teamAddressField = (EditText)findViewById(R.id.teamAddressField);
+    public void onOpenInGoogleMaps(View view) {
+        EditText teamAddressField = (EditText)findViewById(R.id.teamAddressField);
 
-        //Set the Open in Google Maps button's on click listener to open google map with the
-        //postal code specified in the Address field as the address
-        openGoogleMapsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Create a Uri from an intent string. Use the result to create an Intent.
-                Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddressField.getText());
+        //Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddressField.getText());
 
-                //Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        //Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
 
-                //Make the Intent explicit by setting the Google Maps package
-                mapIntent.setPackage("com.google.android.apps.maps");
+        //Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
 
-                //Attempt to start an activity that can handle the Intent
-                startActivity(mapIntent);
-            }
-        });
+        //Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void onSetAvatarButton(View view) {
+        //Application context and activity
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        //Getting the Avatar Image we show to our users
+        ImageView avatar = (ImageView) findViewById(R.id.avatar);
+
+        //Figuring out the correct image
+        String drawableName = "ic_logo_00.png";
+        switch (data.getIntExtra("imageID", R.id.teamid00)) {
+            case R.id.teamid00:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.teamid01:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.teamid02:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.teamid03:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.teamid04:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.teamid05:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+        avatar.setImageResource(resID);
     }
 }
